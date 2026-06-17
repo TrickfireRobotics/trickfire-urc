25788788bb66bab330fb41a7519643f424f510d0
Matej Stastny <matysta@outlook.com>
2026-06-17 13:32:20 -0700

    docs: fixed site base in astro config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-    site: 'https://trickfirerobotics.github.io',
+    site: 'https://docs.trickfirerobotics.com',
     base: '/urc',
     srcDir: './',
     integrations: [
